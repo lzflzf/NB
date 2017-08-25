12c00a914013ea827a7af3928ec1e5c84fe9a6cf
peixunfahan <peixunfahan@peixunfahan-PC>
2017-08-25 10:57:16 +0800

    包名不要包含大写字母，更不用包含中文

--- src/main/java/com/lizhifeng/study/nb/NB.java
+++ src/main/java/com/lizhifeng/study/nb/NB.java
@@ -1,4 +1,4 @@
-package com.lizhifeng.study.NB;
+package com.lizhifeng.study.nb;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,25 +11,50 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-
+import java.util.Iterator;
 
 public class NB {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			ServerSocketChannel ssChannel = ServerSocketChannel.open();
-			SocketAddress local = new InetSocketAddress(9996);
+			SocketAddress local = new InetSocketAddress(9990);
 			ssChannel.socket().bind(local);
 			ssChannel.configureBlocking(true);
 
 			new Thread(new Client(), "client-thread1").start();
 			
+			Selector selector = Selector.open() ;
+
 			while (true) {
 				SocketChannel sc = ssChannel.accept();
+				
 				sc.configureBlocking(false);
+				
+				SelectionKey key = sc.register(selector, SelectionKey.OP_READ) ;
+				
+				selector.select() ;		
+				Iterator<SelectionKey> iterator = selector.selectedKeys().iterator() ;
+				
+				while(iterator.hasNext())
+				{
+					SelectionKey selectionkey = iterator.next() ;					
+					System.out.println(selectionkey.interestOps());				
+				} 
+				
+				try {
+					Thread.sleep(5*1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
 				Handler handler = new Handler(sc);
 				new Thread(handler, "handler-thread").start();
 			}
@@ -54,9 +79,9 @@ class Handler implements Runnable {
 		ByteBuffer Buffer = ByteBuffer.allocate(1024);
 		while (true) {
 			try {
-				Buffer.position(0) ;
+				Buffer.position(0);
 				socketchannel.read(Buffer);
-				System.out.println(Buffer.toString());
+				// System.out.println(Buffer.toString());
 				// Buffer.position(0) ;
 				socketchannel.write(Buffer);
 			} catch (IOException e) {
@@ -75,13 +100,13 @@ class Client implements Runnable {
 		try {
 
 			try {
-				Thread.sleep(10 * 1000);
+				Thread.sleep(1 * 1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
-			Socket socket = new Socket("127.0.0.1", 9996);
+			Socket socket = new Socket("127.0.0.1", 9990);
 
 			while (true) {
 				OutputStream OutputStream = socket.getOutputStream();
@@ -98,8 +123,8 @@ class Client implements Runnable {
 				String readnewline;
 				while ((readnewline = bufferedreader.readLine().trim())
 						.length() > 0) {
-					System.out.println(readnewline);
-					break ;
+					// System.out.println(readnewline);
+					break;
 				}
 			}
 		} catch (UnknownHostException e) {
